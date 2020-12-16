817c086527dc7d51d4d5ff6f02d979d0c3a6141b
tom-pollak <tompollak1000@gmail.com>
2020-12-16 03:32:37 +0000

    added debug.log as outpit

--- cron.sh
+++ cron.sh
@@ -2,5 +2,4 @@ git pull
 #Xvfb :99 -ac &
 #export DISPLAY=:99
 source ~/each-way-matcher/.env
-xvfb-run -a python3 ~/each-way-matcher/run.py
-
+xvfb-run -a python3 ~/each-way-matcher/run.py > debug.log
