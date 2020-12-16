47c46e4c5e33f9c6794cf47ddecdd9f922621d79
Tom Pollak <tompollak1000@gmail.com>
2020-12-16 04:39:44 +0000

    added stbuf to flush prints

--- cron.sh
+++ cron.sh
@@ -1,4 +1,4 @@
 git pull > /dev/null
 #Xvfb :99 -ac &
 #export DISPLAY=:99
-xvfb-run -a python3 ~/each-way-matcher/run.py > debug.log
+xvfb-run -a stdbuf -oL python3 ~/each-way-matcher/run.py > debug.log
