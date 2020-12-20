0a648fa2c43f87087cff4c9c0372a2bcf75f1f4e
Tom Pollak <tompollak1000@gmail.com>
2020-12-20 18:29:42 +0000

    renamed cron to run.sh

--- cron.sh
+++
@@ -1,5 +0,0 @@
-cd ~/each-way-matcher
-git pull > /dev/null
-xvfb-run -a python3 run.py & #&> debug.log &
-wait
-#./update_returns.sh
--- update_returns.sh
+++ update_returns.sh
@@ -1,4 +1,4 @@
 git pull > /dev/null
-python3 graph.py
+python3 graph.py > /dev/null
 git commit -a -m "update returns" > /dev/null
 git push > /dev/null
