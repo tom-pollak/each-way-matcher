2b2640ea8e02a508e1e616a9721e8948a4544aab
Tom Pollak <tompollak1000@gmail.com>
2021-01-06 18:57:05 +0000

    added stake to stake is too small

--- sporting_index.py
+++ sporting_index.py
@@ -144,7 +144,7 @@ def sporting_index_bet(driver, race, retry=False, make_betfair_ew=False):
                                                 race['lay_odds_place'],
                                                 race['place'], race['balance'])
     if race['ew_stake'] < 0.1:
-        print('Stake is too small')
+        print(f"Stake is too small: {race['ew_stake']}")
         return race, False
 
     if float(cur_odd_price) == float(race['horse_odds']):
