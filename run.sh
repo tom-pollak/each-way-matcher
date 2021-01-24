357a665e841f298e23ce48afeeecc4e7f56e01e9
tom-pollak <tompollak1000@gmail.com>
2021-01-24 22:49:37 +0000

    update returns

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ cd $(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)
 > debug.log
 git pull -q
 
-xvfb-run -a python3 main.py & | tee -a debug.log backup.log > /dev/null
+xvfb-run -a python3 main.py | tee -a debug.log backup.log > /dev/null &
 wait
 
 git pull -q
