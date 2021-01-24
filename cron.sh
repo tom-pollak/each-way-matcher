b0b9f020c49054014061682b4e45731ce14bf86d
Tom Pollak <tompollak1000@gmail.com>
2021-01-24 18:24:08 +0000

    merged cron.sh ru.sh

--- cron.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd $(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)
-./run.sh | tee -a debug.log backup.log > /dev/null
--- run.sh
+++ run.sh
@@ -4,9 +4,9 @@ cd $(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)
 > debug.log
 git pull -q
 
-xvfb-run -a python3 main.py & 
+xvfb-run -a python3 main.py & | tee -a debug.log backup.log > /dev/null
 wait
 
 git pull -q
-python3 stats.py
+python3 stats.py | tee -a debug.log backup.log > /dev/null
 git commit -q -a -m "update returns" && git push -q
