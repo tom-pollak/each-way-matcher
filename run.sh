e6c7e6ecb5621b332b241f66e5fd6de9c466cf7f
Tom Pollak <tompollak1000@gmail.com>
2021-01-24 15:19:51 +0000

    merged run.sh and update_returns.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,9 @@
 cd $(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)
 > debug.log
 git pull -q
+
 xvfb-run -a python3 main.py & 
 wait
-./update_returns.sh
+python3 stats.py
+
+git pull -q && git commit -q -a -m "update returns" && git push -q
