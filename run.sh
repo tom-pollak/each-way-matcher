555f6e6b6cd8f712da719391e6af6eb76debd8fb
Tom Pollak <tompollak1000@gmail.com>
2021-02-05 05:53:01 +0000

    removed wait from run.sh

--- run.sh
+++ run.sh
@@ -5,8 +5,7 @@ cd $(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)
 echo -e "\n" 2> /dev/null 3<backup.log 3<&- >>backup.log
 git pull -q
 
-xvfb-run -a python3 main.py | tee -a debug.log backup.log > /dev/null &
-wait
+xvfb-run -a python3 main.py | tee -a debug.log backup.log > /dev/null
 
 git pull -q
 python3 stats.py | tee -a debug.log backup.log > /dev/null
