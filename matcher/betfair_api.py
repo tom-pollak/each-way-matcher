bf2b9e1c91e8eb59387acbd90dfa3b7ae73f747d
Tom Pollak <tompollak1000@gmail.com>
2021-03-03 01:43:29 +0000

    updated request logging

--- matcher/betfair_api.py
+++ matcher/betfair_api.py
@@ -65,7 +65,6 @@ def call_api(jsonrpc_req, headers, url=betting_url):
     except error.HTTPError:
         print('Not a valid operation' + str(url))
     except error.URLError as e:
-        print(e.reason)
         print('No service available at ' + str(url))
     raise ValueError('API request failed')
 
