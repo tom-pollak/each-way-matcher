038347fafa539d4b27bb6cc574deb694efdad8de
Tom Pollak <tompollak1000@gmail.com>
2021-03-27 19:50:44 +0000

    added tab as local

--- matcher/extra_places.py
+++ matcher/extra_places.py
@@ -9,7 +9,6 @@ from .william_hill import get_william_hill_page, scrape_odds_william_hill
 enabled_sites = {
     "William Hill": {"get": get_william_hill_page, "scrape": scrape_odds_william_hill}
 }
-tab = 0
 
 
 def update_odds_df(odds_df, horses, bookie):
@@ -38,11 +37,12 @@ def get_tab_id(bookies_df, venue, time, site):
     return bookies_df.at[(venue, time), (site, "tab_id")]
 
 
-def create_tab_id(driver, bookies_df, venue, time, site):
+def create_tab_id(driver, bookies_df, venue, time, site, tab):
     driver.execute_script("""window.open("https://google.com","_blank");""")
     driver.switch_to.window(driver.window_handles[tab])
     bookies_df.at[(venue, time), (site, "tab_id")] = tab
     tab += 1
+    return tab
 
 
 def get_odds(driver, odds_df, bookies_df):
@@ -50,6 +50,7 @@ def get_odds(driver, odds_df, bookies_df):
 
 
 def run_extra_places():
+    tab = 0
     races_df, odds_df, bookies_df, horse_id_df = generate_df()
     driver = setup_selenium()
     setup_betfair_scrape(driver, tab=0)
@@ -58,22 +59,22 @@ def run_extra_places():
     ):
         sites = [site for site in enabled_sites if site in horse_id_df[index].columns]
         if sites:
-            create_tab_id(
-                driver, bookies_df, index[0], index[1], "Betfair Exchange Win"
+            tab = create_tab_id(
+                driver, bookies_df, index[0], index[1], "Betfair Exchange Win", tab
             )
             get_site(driver, race.win_market_id, tab)
             horses = scrape_odds_betfair(driver, tab)
             update_odds_df(odds_df, horses, "Betfair Exchange Win")
 
-            create_tab_id(
-                driver, bookies_df, index[0], index[1], "Betfair Exchange Place"
+            tab = create_tab_id(
+                driver, bookies_df, index[0], index[1], "Betfair Exchange Place", tab
             )
             get_site(driver, race.place_market_id, tab)
             horses = scrape_odds_betfair(driver, tab)
             update_odds_df(odds_df, horses, "Betfair Exchange Place")
 
             for site in sites:
-                create_tab_id(driver, bookies_df, index[0], index[1], site)
+                tab = create_tab_id(driver, bookies_df, index[0], index[1], site, tab)
                 sites[site]["get"](driver, index[0], index[1], tab)
                 horses = sites[site]["scrape"](driver, tab)
                 update_odds_df(odds_df, horses, site)
