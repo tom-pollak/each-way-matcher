e1dda5533341370cc6b732458acb773748cfb6ac
Tom Pollak <tompollak1000@gmail.com>
2021-04-08 18:05:16 +0100

    removed unused import

--- matcher/setup.py
+++ matcher/setup.py
@@ -10,7 +10,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import (
-    WebDriverException,
     TimeoutException,
     ElementClickInterceptedException,
 )
