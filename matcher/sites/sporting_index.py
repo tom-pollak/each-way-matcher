b34ca7a60ef05ab0cbf48fa5717355eaa2481b2a
Tom Pollak <tompollak1000@gmail.com>
2021-04-24 17:53:20 +0100

    removed timeout logging

--- matcher/sites/sporting_index.py
+++ matcher/sites/sporting_index.py
@@ -81,8 +81,6 @@ def make_sporting_index_bet(driver, race):
         return True
 
     except WebDriverException as e:
-        print(e)
-        print(traceback.format_exc())
         return False
 
 
