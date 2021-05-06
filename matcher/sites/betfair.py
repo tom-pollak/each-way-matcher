808b03193fe1a900f84a4bfbce7f0f628dc97624
Tom Pollak <tompollak1000@gmail.com>
2021-05-06 15:14:38 +0100

    fixed variable

--- matcher/sites/betfair.py
+++ matcher/sites/betfair.py
@@ -294,7 +294,7 @@ def get_balance():
 def get_exposure():
     account_url = "https://api.betfair.com/exchange/account/json-rpc/v1"
     exposure_req = '{"jsonrpc": "2.0", "method": "AccountAPING/v1.0/getAccountFunds"}'
-    exposure_res = call_api(balance_req, url=account_url)
+    exposure_res = call_api(exposure_req, url=account_url)
     exposure = balance_res["result"]["exposure"]
     return exposure
 
