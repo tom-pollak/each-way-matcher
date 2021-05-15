7299de5fa079072e035ffdbcadfb18e2d0e1ede6
Tom Pollak <tompollak1000@gmail.com>
2021-05-15 14:27:16 +0100

    removed expected value

--- matcher/calculate.py
+++ matcher/calculate.py
@@ -293,15 +293,12 @@ def calculate_expected_return(
     exp_growth = (1 + win_profit / total_balance) ** win_prob * (
         1 + place_profit / total_balance
     ) ** place_prob * (1 + lose_profit / total_balance) ** lose_prob - 1
-    exp_value = (
-        win_prob * win_profit + place_prob * place_profit + lose_prob * lose_profit
-    )
     if isinstance(exp_growth, complex):
         print(
             "ERROR: exp_growth complex - a profit loses more than is in total_balance"
         )
-        return exp_value, 0, 0
-    return exp_value, exp_growth, exp_growth * total_balance
+        return 0, 0
+    return exp_growth, exp_growth * total_balance
 
 
 def round_odd(odd):
