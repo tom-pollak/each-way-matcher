50715c7917439ee1e97dab6634749b1f7299e187
Tom Pollak <tompollak1000@gmail.com>
2021-06-15 14:39:26 +0100

    added get horse position and places paid

---
+++ matcher/race_results.py
@@ -0,0 +1,47 @@
+import os
+import requests
+from datetime import datetime
+from dotenv import load_dotenv
+
+from .exceptions import MatcherError
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__) + "/../")
+load_dotenv(os.path.join(BASEDIR, ".env"))
+RAPID_API_KEY = os.environ.get("RAPID_API_KEY")
+
+
+def call_api(url, params=None):
+    headers = {
+        "x-rapidapi-host'": "horse-racing.p.rapidapi.com",
+        "x-rapidapi-key": RAPID_API_KEY,
+    }
+    res = requests.request("GET", url, headers=headers, params=params)
+    if res.status_code == 200:
+        return res.json()
+    print(res.text)
+    raise MatcherError
+
+
+def get_race_id(venue, time):
+    params = {"date": str(time)}
+    res = call_api("https://horse-racing.p.rapidapi.com/results", params=params)
+    for race in res:
+        race_time = datetime.strptime(race["date"], "%Y-%m-%d %H:%M:%S")
+        if race["course"] == venue and time == race_time:
+            return race["id_race"]
+    raise MatcherError
+
+
+def get_position(venue, time, horse_name, places_paid):
+    try:
+        race_id = get_race_id(venue, time)
+        res = call_api("https://horse-racing.p.rapidapi.com/race/%s" % race_id)
+    except MatcherError:
+        return None
+
+    for horse in res["horses"]:
+        if horse_name in horse["horse"]:
+            if horse["non_runner"] == "1":
+                return "Non-runner"
+            return horse["position"]
+    return None
