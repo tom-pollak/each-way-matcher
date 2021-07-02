9433c0b6f04ee205337afef0b619e3419a363b7b
Tom Pollak <tompollak1000@gmail.com>
2021-07-02 01:34:56 +0100

    dosent use matcher error to indicate errors

--- matcher/race_results.py
+++ matcher/race_results.py
@@ -3,8 +3,6 @@ import requests
 from datetime import datetime
 from dotenv import load_dotenv
 
-from .exceptions import MatcherError
-
 BASEDIR = os.path.abspath(os.path.dirname(__file__) + "/../")
 load_dotenv(os.path.join(BASEDIR, ".env"))
 RAPID_API_KEY = os.environ.get("RAPID_API_KEY")
@@ -18,13 +16,15 @@ def call_api(url, params=None):
     res = requests.request("GET", url, headers=headers, params=params)
     if res.status_code == 200:
         return res.json()
-    print(res.text)
-    raise MatcherError
+    return None
 
 
 def get_race_id(venue, time):
     params = {"date": str(time)}
     res = call_api("https://horse-racing.p.rapidapi.com/results", params=params)
+    if res is None:
+        return None
+
     for race in res:
         race_time = datetime.strptime(race["date"], "%Y-%m-%d %H:%M:%S")
         if (venue in race["course"] or race["course"] in venue) and time == race_time:
@@ -33,19 +33,19 @@ def get_race_id(venue, time):
     races = []
     for race in res:
         races.append((race["course"], race["date"]))
-    raise MatcherError(races)
+    return races
 
 
 def get_position(venue, time, horse_name):
-    try:
-        race_id = get_race_id(venue, time)
-    except MatcherError as venues:
-        print("Course not found: %s, %s\n%s" % (venue, time, venues))
+    race_id = get_race_id(venue, time)
+    if race_id is None:
+        return "Rate limit exceeded"
+    elif isinstance(race_id, list):
+        print("\nCourse not found: %s, %s\n%s" % (venue, time, race_id))
         return None
 
-    try:
-        res = call_api("https://horse-racing.p.rapidapi.com/race/%s" % race_id)
-    except MatcherError:
+    res = call_api("https://horse-racing.p.rapidapi.com/race/%s" % race_id)
+    if res is None:
         return "Rate limit exceeded"
 
     for horse in res["horses"]:
